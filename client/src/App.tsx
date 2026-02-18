35e80a5d1624b77a6d0135676317cb442606b9f0
VladlenKhan <gitvlad777@gmail.com>
2026-02-18 14:59:29 +0300

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,14 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar />
       <Hero />
-      <MainContent/>
-      <Footer/>
-      {/* здесь будет остальной контент */}
+      <MainContent />
+      <Footer />
     </div>
   );
 }
